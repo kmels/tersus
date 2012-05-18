10fe151726a0faad25ad39c11f6e6c7050d5159c
Carlos Lopez-Camey <c.lopez@kmels.net>
2012-05-18 18:52:22 +0200

    Models TApplication, User, File, Email and Permission

--- Model.hs
+++ Model.hs
@@ -10,5 +10,15 @@ import Database.Persist.Quasi
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
+data WriteMode = Override | AppendToFile | Create | Delete deriving (Show, Eq, Enum)
+
+data ReadMode = FileMetadata | GetContent | Pagination deriving (Show, Eq, Enum)
+
+data TersusResultCode = Success | InexistentFile | NotEnoughPrivileges | DirectoryNotEmpty | OutOfRange deriving (Show, Eq, Enum)
+
+data TersusResult = TersusResult Int TersusResultCode
+
+data MessageResult = Read | NotRead
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
