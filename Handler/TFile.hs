ed41aaef783fda65f1169928c3d7de5d93f7bb00
Carlos Lopez-Camey <c.lopez@kmels.net>
2012-05-19 15:03:24 +0200

    proof of concept: writefile form

--- Handler/TFile.hs
+++ Handler/TFile.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, MultiParamTypeClasses,
+    OverloadedStrings, TypeFamilies #-}
 module Handler.TFile where
 
+import Control.Applicative ((<$>), (<*>))
+import Data.Text (Text)
 import Import
+import Yesod
+import Yesod.Form.Jquery
 
 {- Handler methods for operations on files. -}
 
@@ -21,20 +27,42 @@ getFileR userId appKey path = do
      <h1>Path: #{show path}
             |]
 
+
+-- Form to handle writing to files. 
+--
+-- The **HTML** form (i.e. GET) is only used for testing reasons, the value is used to handle
+-- requests coming from REST (i.e. POST)
+--
+-- 
+--Use standard english (to i18n, supply a translation function)
+
+data FileContent = FileContent{
+  fileContentContent :: Text
+} deriving Show
+ 
+writeFileForm :: Html -> MForm App App (FormResult FileContent, Widget)
+writeFileForm = renderDivs $ FileContent
+    <$> areq textField "Content" Nothing
+    
+-- This won't really exist after, it is used for testing purposes only.
 getWriteFileR :: Int -> TAppKey -> [Text] -> Handler RepHtml
 getWriteFileR userId appKey path = do
-  defaultLayout $ do
-    [whamlet|
-     <h1>User id: #{userId}
-     <h1>AppKey: #{appKey}
-     <h1>Path: #{show path}
-            |]
-            
+  --get the form
+  (formWidget, enctype) <- generateFormPost writeFileForm
+  defaultLayout [whamlet|
+     <h1>Dear user #{userId}, you are going to write content to file #{show path}
+     <form method=post action=@{WriteFileR userId appKey path} enctype=#{enctype}>
+                  ^{formWidget}
+                  <input type=submit>
+     |]
+
 postWriteFileR :: Int -> TAppKey -> [Text] -> Handler RepHtml
 postWriteFileR userId appKey path = do
-  defaultLayout $ do 
-    [whamlet|
-     <h1>User id: #{userId}
-     <h1>AppKey: #{appKey}
-     <h1>Path: #{show path}
-            |]
+  ((result, widget), enctype) <- runFormPost writeFileForm
+  case result of
+    FormSuccess fc -> defaultLayout $ do 
+      [whamlet|
+       <h1>You wrote: #{show $ fileContentContent fc}
+       <h3>in #{show path}
+              |]
+    _ -> defaultLayout [whamlet|<p>Invalid input!|]
