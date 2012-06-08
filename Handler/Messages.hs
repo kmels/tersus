e439e37b97f42756c0ad0251db914300c8462fdd
Ernesto Rodriguez <ernesto4160@gmail.com>
2012-06-08 22:06:02 +0200

    Created api functions for Tersus Messaging system without Cloud Haskell yet

--- Handler/Messages.hs
+++ Handler/Messages.hs
@@ -16,14 +16,6 @@ import Model.TMessage
 import Data.Time.Clock (getCurrentTime)
 import System.IO.Unsafe (unsafePerformIO)
 
--- -- registerUser :: String -> String -> IO ()
--- registerUser user app = do
---              master <- getYesod
---              mailBoxes' <- mailBoxes master
---              mailBox <- liftIO newEmptyMVar
---              liftIO $ H.insert mailBoxes' (user,app) mailBox
---              jsonToRepJson $ encode $ TersusResult 3 Import.Success
-
 dummyUser = User (T.pack "neto") (Just (T.pack "1234")) []
 
 -- This is a dummy datatype only to show that this works
@@ -36,32 +28,66 @@ dummyMsg = TMessage dummyUser dummyUser dummyApp dummyApp (T.pack "Alonso")
 
 dummyInstance = AppInstance "neto" "emacs"
 
+-- Load the mailbox belonging to the given app instance. Or None
+-- if the mailBox has not been created or is not in this server
+loadMailbox :: App -> AppInstance -> IO (Maybe (MVar [TMessage]))
+loadMailbox master appInstance = do
+            mailBoxes' <- return $ mailBoxes master
+            H.lookup mailBoxes' appInstance
+
+-- Create a mailbox accesible to the given app instance
+createMailbox :: App -> AppInstance -> IO ()
+createMailbox master appInstance = loadMailbox master appInstance
+              >>= \m -> case m of
+                  Nothing -> mkMailbox
+                  _ -> return ()
+              where
+                mkMailbox = do
+                          mailBoxes' <- return $ mailBoxes master
+                          mailBox <- newEmptyMVar
+                          H.insert mailBoxes' appInstance mailBox
+
+-- Get the messages from the mailbox belonging to the 
+-- given app instance
+getMessages :: App -> AppInstance -> IO (Maybe [TMessage])
+getMessages master appInstance =
+            loadMailbox master appInstance
+            >>= openMailBox
+            where
+                openMailBox (Just mail) = takeMVar mail 
+                                          >>= return . Just
+                openMailBox Nothing = return Nothing
+
+-- Add a message to the mailbox belonging to the given
+-- App instance. Return true if the message was written
+-- or false if such mailbox dosen't exist
+writeMessage :: App -> AppInstance -> TMessage -> IO Bool
+writeMessage master appInstance message = 
+             loadMailbox master appInstance
+             >>= addMessage
+             where
+                addMessage (Just var)  =  isEmptyMVar var >>= \b -> case b of
+                           True -> putMVar var [message] >> return True
+                           False -> modifyMVar_ var (return . (:) message) 
+                                    >> return True
+                addMessage Nothing = return False
+             
+
+-- Dummy test functions, will not exist in future releases
 getInitMVarR = do 
             master <- getYesod
-            mailBoxes' <- return $ mailBoxes master
-            mailBox <- liftIO newEmptyMVar
-            liftIO $ H.insert mailBoxes' dummyInstance mailBox
+            liftIO $ createMailbox master dummyInstance
             jsonToRepJson $ encode $ TersusResult 3 Import.Success
 
 getMessageR :: Handler RepJson
 getMessageR = do
             master <- getYesod
-            mailBoxes' <- return $ mailBoxes master
-            Just mailBox <- liftIO $ H.lookup mailBoxes' dummyInstance
-            msgs <- liftIO $ takeMVar mailBox
+            Just msgs <- liftIO $ getMessages master dummyInstance
             jsonToRepJson $ encode msgs
 
 getSetMVarR :: Handler RepJson
 getSetMVarR = do
             master <- getYesod
-            mailBoxes' <- return $ mailBoxes master
-            Just mailBox <- liftIO $ H.lookup mailBoxes' dummyInstance
-            liftIO $ addMessage mailBox dummyMsg
+            liftIO $ writeMessage master dummyInstance dummyMsg
             jsonToRepJson $ encode $ TersusResult 3 Import.Success
-
-            where
-                addMessage var msg =  isEmptyMVar var >>= \b -> case b of
-                                     True -> putMVar var [msg]
-                                     False -> modifyMVar_ var (\x -> return (msg:x))
-            
              
