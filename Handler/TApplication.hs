0266a96e160063fefab554cdd2cae08345566d07
Carlos Lopez-Camey <c.lopez@kmels.net>
2012-06-08 19:37:41 +0200

    Closes issue #7
     - Add an `identifier` field to `TApplication`, fix temporal form in /app/register
    
     - Rename `AppKey` to `AccessToken` in requests /file/*. `AppKey` is known only by the app, `AccessToken` is public to the user and app.

--- Handler/TApplication.hs
+++ Handler/TApplication.hs
@@ -17,13 +17,15 @@ import           Codec.Crypto.AES.Random(randBytes)
 import           Control.Arrow     (first, (&&&), (***))
 import qualified Data.ByteString          as B
 import           Data.Time.Clock   (getCurrentTime)
-import           qualified Data.Text as T
+import qualified Data.Text as T
+
 --getAppKey =
 
 -- The data type that is expected from registerAppForm
 
 data AppLike = AppLike {
   appLikeName :: Text
+  , appLikeIdentifier :: Text
   , appLikeDescription :: Text
   , appLikeRepositoryURL :: Maybe Text
   , appLikeContactEmail :: Text
@@ -32,28 +34,23 @@ data AppLike = AppLike {
 -- A monadic form
 registerAppForm :: Html -> MForm App App (FormResult AppLike,Widget)
 registerAppForm extra = do
-  (nameRes, nameView) <- mreq textField "{-This is not used-}" Nothing
-  (descriptionRes, descriptionView) <- mreq textField "{-This is not used-}" Nothing
-  (repositoryUrlRes, repositoryUrlView) <- mopt textField "RepositoryURL" Nothing
-  (contactEmailRes, contactEmailView) <- mreq textField "ContactEmail" Nothing
-  let appLikeResult = AppLike <$> nameRes <*> descriptionRes <*> repositoryUrlRes <*> contactEmailRes
+  (nameRes, nameView) <- mreq textField "{- not used -}" Nothing
+  (descriptionRes, descriptionView) <- mreq textField "{- not used-}" Nothing
+  (repositoryUrlRes, repositoryUrlView) <- mopt textField "{- not used -}" Nothing
+  (contactEmailRes, contactEmailView) <- mreq textField "{- not used -}" Nothing
+  (identifierRes, identifierView) <- mreq textField "{- not used -}" Nothing
+  let appLikeResult = AppLike <$> nameRes <*> identifierRes <*> descriptionRes <*> repositoryUrlRes <*> contactEmailRes
   let widget = do
             toWidget [lucius|
             ##{fvId contactEmailView} {
             width: 3em;
             }|]
             [whamlet|#{extra}
-              <p>
-              App name: #
-              ^{fvInput nameView}
-              <p>
-              Description: : #
-              ^{fvInput descriptionView}
-              <p>
-              If open source, repository url is: ^{fvInput repositoryUrlView} #
-              <p>
-              contact email ^{fvInput contactEmailView}
-              <p>
+              <p> App name: # ^{fvInput nameView}              
+              <p> Url: ^{fvInput identifierView}
+              <p> Description: : ^{fvInput descriptionView}
+              <p> If open source, repository url is: ^{fvInput repositoryUrlView} #
+              <p> contact email ^{fvInput contactEmailView}
              |]
   return (appLikeResult, widget)
 
@@ -74,13 +71,13 @@ postRegisterTAppR = do
     FormSuccess appLike -> do
       -- get data from the form
       let 
-        (appName,(appDescription,(appRepositoryUrl,appContactEmail))) = (appLikeName &&& appLikeDescription &&& appLikeRepositoryURL &&& appLikeContactEmail) appLike
+        (appName,(appDescription,(appRepositoryUrl,(appContactEmail,appIdentifier)))) = (appLikeName &&& appLikeDescription &&& appLikeRepositoryURL &&& appLikeContactEmail &&& appLikeIdentifier) appLike
                 
       creationDate <- liftIO getCurrentTime --ask date
       appKey <- liftIO $ newAppRandomKey  --create a new appkey
       
       --insert in database
-      appid <- runDB $ insert $ TApplication appName appDescription appRepositoryUrl appContactEmail creationDate appKey
+      appid <- runDB $ insert $ TApplication appName appIdentifier appDescription appRepositoryUrl appContactEmail creationDate appKey
       
       defaultLayout $ [whamlet|
       <h1>Received, your generated key: #{appKey}
