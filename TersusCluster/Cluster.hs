1a09f8a1c0d283d447937d94d2b8f14fd47cf40c
Carlos Lopez-Camey <c.lopez@kmels.net>
2012-06-17 23:16:31 +0200

    Eliminate Warnings

--- TersusCluster/Cluster.hs
+++ TersusCluster/Cluster.hs
@@ -1,27 +1,28 @@
 module TersusCluster.Cluster where
 
-import Prelude
-import Control.Monad (forever)
-import Control.Concurrent (forkIO,threadDelay)
-import Remote
-import Model.TMessage
-import Remote.Call (mkClosureRec, mkClosure)
-import Model
-import Data.Text as T
-import System.IO.Unsafe (unsafePerformIO)
-import Data.Time.Clock (getCurrentTime)
-import Control.Monad.Trans (liftIO)
-import Data.HashTable as H
-import Network.Wai.Handler.Warp (runSettings, defaultSettings, settingsPort)
-import Application
-import Control.Concurrent.MVar 
-import Settings (parseExtra)
-import Yesod.Default.Config (fromArgs)
-import Yesod.Default.Main (defaultMain)
-import System.Posix.Signals
-import System.Exit
+import           Application
+import           Control.Concurrent (forkIO,threadDelay)
+import           Control.Concurrent.MVar
+import           Control.Monad (forever)
+import           Control.Monad.Trans (liftIO)
 import qualified Data.Binary as B
-import Data.Typeable.Internal (Typeable)
+import           Data.HashTable as H
+import           Data.Text as T
+import           Data.Time.Clock (getCurrentTime)
+import           Data.Typeable.Internal (Typeable)
+import           GHC.Int(Int32(..))
+import           Model
+import           Model.TMessage
+import           Network.Wai.Handler.Warp (runSettings, defaultSettings, settingsPort)
+import           Prelude
+import           Remote
+import           Remote.Call (mkClosureRec, mkClosure)
+import           Settings (parseExtra)
+import           System.Exit
+import           System.IO.Unsafe (unsafePerformIO)
+import           System.Posix.Signals
+import           Yesod.Default.Config (fromArgs)
+import           Yesod.Default.Main (defaultMain)
 
 data TersusClusterSignals = TKill deriving Typeable
 
@@ -50,23 +51,23 @@ matchTMessage :: MatchM TMessage ()
 matchTMessage = match return
 
 -- Hash function for a user application combo instance
-hashUserApp (AppInstance username application)  = H.hashString $ username ++ application
+hashUserApp (AppInstance username' app')  = H.hashString $ username' ++ app' -- 
 
 -- Process that runs Tersus and Yesod in development mode
 createTersusDevelInstance :: ProcessM ()
 createTersusDevelInstance = do addresses <- liftIO $ H.new (==) hashUserApp
                                mailBoxes <- liftIO $ H.new (==) hashUserApp
-                               liftIO $ do (port, app) <- getApplicationDev addresses mailBoxes    
-                                           forkIO $ runSettings defaultSettings
+                               _ <- liftIO $ do (port, tAppInstance) <- getApplicationDev addresses mailBoxes    
+                                                forkIO $ runSettings defaultSettings
                                                     { settingsPort = port
-                                                    } app
+                                                    } tAppInstance
                                runTersusMessaging addresses mailBoxes
 
 -- Process that runs Tersus and Yesod in producction mode
 createTersusInstance :: ProcessM ()
 createTersusInstance = do addresses <- liftIO $ H.new (==) hashUserApp
                           mailBoxes <- liftIO $ H.new (==) hashUserApp
-                          liftIO $ forkIO $ defaultMain (fromArgs parseExtra) $ makeApplicationWrapper addresses mailBoxes
+                          _ <- liftIO $ forkIO $ defaultMain (fromArgs parseExtra) $ makeApplicationWrapper addresses mailBoxes
                           runTersusMessaging addresses mailBoxes
 
 -- Function that handles messaging among the multiple Tersus instances
@@ -80,16 +81,18 @@ killSignal = match return
 createTersusInstance' :: ProcessM ()
 createTersusInstance' = do
                      peers <- receiveWait [matchProcesses]
-                     myId <- getSelfPid
-                     peers' <- return $ rmItem myId peers
-                     forever $ do mapM_ (\id -> send id dummyMsg) peers
+--kmels: THIS WAS NOT BEING USED SO I COMMENTED IT
+--                     myId <- getSelfPid
+--kmels: THIS WAS NOT BEING USED SO I COMMENTED IT
+--                     peers' <- return $ rmItem myId peers 
+                     forever $ do mapM_ (\tid -> send tid dummyMsg) peers
                                   mapM_ (\_ -> doRecieve) peers
-
                      where                        
-                        rmItem _ [] = []
-                        rmItem a (x:xs) 
-                               | a == x = xs
-                               | otherwise = x : (rmItem a xs)
+--kmels: THIS WAS NOT BEING USED SO I COMMENTED IT
+--                        rmItem _ [] = []
+--                        rmItem a (x:xs) 
+--                               | a == x = xs
+--                               | otherwise = x : (rmItem a xs)
                         doRecieve = do (TMessage _ _ _ _ msg) <- receiveWait [matchTMessage]
                                        say $ "I got: " ++ (T.unpack msg)
 --
@@ -99,12 +102,14 @@ remotable ['createTersusInstance,'createTersusDevelInstance]
 
 -- Functions to initialize all the tersus nodes and distribute the ProcessId of such nodes
 -- Development and producction version of the function
+initTersusCluster :: String -> ProcessM ()
 initTersusCluster "T2" = receiveWait []
 initTersusCluster "T1" = do 
                             peers <- getPeers
                             t2s <- return $ findPeerByRole peers "T2"
                             pids <- mapM (\p -> (spawn p $(mkClosure 'createTersusInstance)))  t2s
                             mapM_ (\p -> send p pids) pids
+initTersusCluster _ = return ()
 
 sigTermCatch :: MVar Int -> IO ()
 sigTermCatch mVar = putMVar mVar 1 >>
@@ -112,26 +117,30 @@ sigTermCatch mVar = putMVar mVar 1 >>
                     exitWith ExitSuccess
                                 
                     
-
+-- TODO: Add description of what this function does
+initTersusClusterDevel :: String -> ProcessM ()
 initTersusClusterDevel "T2" = receiveWait [killSignal] >> return ()
 initTersusClusterDevel "T1" = do peers <- getPeers
                                  t2s <- return $ findPeerByRole peers "T2"
                                  pids <- mapM (\p -> (spawn p $(mkClosure 'createTersusDevelInstance)))  t2s
                                  mapM_ (\p -> send p pids) pids
                                  killMvar <- liftIO $ newEmptyMVar
-                                 liftIO $ installHandler sigTERM (Catch (sigTermCatch killMvar) ) Nothing
+                                 _ <- liftIO $ installHandler sigTERM (Catch (sigTermCatch killMvar) ) Nothing
                                  _ <- liftIO $ takeMVar killMvar
                                  mapM_ (\p -> send p TKill) pids
                                  return ()
+initTersusClusterDevel _ = return()
 
 -- Initialize Tersus in producction mode running on top of CloudHaskell
 tersusProducction :: IO ()
-tersusProducction = do forkIO $ remoteInit (Just "config/servers") [TersusCluster.Cluster.__remoteCallMetaData] initTersusCluster
-                       remoteInit (Just "config/servers2") [TersusCluster.Cluster.__remoteCallMetaData] initTersusCluster
-                       return ()
+tersusProducction = do 
+  _ <- forkIO $ remoteInit (Just "config/servers") [TersusCluster.Cluster.__remoteCallMetaData] initTersusCluster
+  remoteInit (Just "config/servers2") [TersusCluster.Cluster.__remoteCallMetaData] initTersusCluster
+  return ()
 
 -- Initialize Tersus in development mode running on top of CloudHaskell
 tersusDevel :: IO ()
-tersusDevel = do forkIO $ remoteInit (Just "config/servers") [TersusCluster.Cluster.__remoteCallMetaData] initTersusClusterDevel
-                 remoteInit (Just "config/servers2") [TersusCluster.Cluster.__remoteCallMetaData] initTersusClusterDevel
-                 return ()
+tersusDevel = do 
+  _ <- forkIO $ remoteInit (Just "config/servers") [TersusCluster.Cluster.__remoteCallMetaData] initTersusClusterDevel
+  remoteInit (Just "config/servers2") [TersusCluster.Cluster.__remoteCallMetaData] initTersusClusterDevel
+  return ()
