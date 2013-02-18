3113e85178c704f23fa36c0ea6d1e9e3b36158e5
Carlos López-Camey <c.lopez@kmels.net>
2013-02-18 19:34:40 +0100

    before merging

--- Tersus/DataTypes/Address.hs
+++ Tersus/DataTypes/Address.hs
@@ -1,7 +1,6 @@
 module Tersus.DataTypes.Address where 
 
-import Tersus.DataTypes.Addressable
-import Tersus.DataTypes.AppInstance
+import Tersus.DataTypes
 
 instance Addressable Address where
-    getAppInstance (Address (User _ nickname _ _) (TApp _ id' _ _ _ _ _)) = AppInstance nickname id'
+    getAppInstance (Address (User _ _ nickname _ _) (TApp _ id' _ _ _ _ _)) = AppInstance nickname id'
